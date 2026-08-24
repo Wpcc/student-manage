3b073fb9c6c3b75b30d55dcbf77e477f5a610d3b
wpcc <310624463@qq.com>
2026-08-24 16:23:14 +0800

    feat: add annotation driven validation

---
+++ src/main/java/com/example/studentmanager/NotBlank.java
@@ -0,0 +1,14 @@
+package com.example.studentmanager;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Documented
+public @interface NotBlank {
+  String message() default "字段不能为空";
+}
